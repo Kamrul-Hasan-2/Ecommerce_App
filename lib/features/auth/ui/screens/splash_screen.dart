a3916767d3d23f23b7cb2643f4b465e685f7f186
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2025-01-06 00:04:48 +0600

    Complete Splash Screen

--- lib/features/auth/ui/screens/splash_screen.dart
+++ lib/features/auth/ui/screens/splash_screen.dart
@@ -1,9 +1,11 @@
-import 'package:ecomerce/app/assets_path.dart';
+import 'package:ecomerce/features/auth/ui/screens/verify_email_screen.dart';
+import 'package:ecomerce/features/auth/ui/widgets/app_logo_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  static const String name = '/';
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -11,21 +13,29 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    _moveToNextScreen();
+    super.initState();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacementNamed(context, VerifyEmailScreen.name);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return const Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(24),
+          padding: EdgeInsets.all(24),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Spacer(),
-              SvgPicture.asset(
-                AssetsPath.appLogoSvg,
-                width: 120,
-              ),
-              const Spacer(),
-              const CircularProgressIndicator()
+              Spacer(),
+              AppLogoWidget(),
+              Spacer(),
+              CircularProgressIndicator()
             ],
           ),
         ),
